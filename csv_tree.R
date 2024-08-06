29b8516f15a0fa596d31f2550374a78253f9744d
Wei-Jan Chang <cc9868422@gmail.com>
2024-08-06 18:28:18 +0200

    Update csv_tree.R
    id, parent_id, child_id in a df

--- csv_tree.R
+++ csv_tree.R
@@ -5,15 +5,31 @@ library(jsonlite)
 # Read the CSV file
 df <- read.csv("/Users/wei/Python/neuro/test_files/structure_tree_safe_2017.csv")
 
-# Create a data frame with relevant columns
-data <- data.frame(id = df$id, parent_structure_id = df$parent_structure_id, acronym = df$acronym)
-
-result <- data %>%
+result <- df %>%
   group_by(parent_structure_id) %>%
   reframe(child_id = paste(sort((id)), collapse = ", ")
   ) %>%
   unique()
-typeof(result)
-# 比list 是錯的
 
+tree_csv <- as.data.frame(result)
+
+library(dplyr)
+
+# Read the CSV file
+df <- read.csv("/Users/wei/Python/neuro/test_files/structure_tree_safe_2017.csv")
+
+# Group by 'parent_structure_id' and concatenate 'id' values
+children_df <- df %>%
+  group_by(parent_structure_id) %>%
+  summarise(children = paste(sort(id), collapse = ", "))
+
+# Merge back with the original dataframe to add the children information
+result <- df %>%
+  left_join(children_df, by = c("id" = "parent_structure_id"))
+
+# Select only the relevant columns: 'id', 'parent_structure_id', and 'children'
+result <- result %>%
+  select(id, parent_structure_id, children)
+
+view(result)
 tree_csv <- as.data.frame(result)
