bccb1ab38edaae3ab52b13ab5bae4fcb3ed5091d
Wei-Jan Chang <98678790+WeiJanChang@users.noreply.github.com>
2025-01-16 16:45:32 +0000

    find differences between json and csv files

--- difference_tree_structure.R
+++ difference_tree_structure.R
@@ -1,3 +1,5 @@
+library(stringr) # to run mutate
+
 # compare two df
 identical(tree_json, tree_csv)
 all.equal(tree_json, tree_csv)
@@ -21,13 +23,13 @@ only_in_csv_childid <- setdiff(tree_csv$children, tree_json$children)
 # show the difference in df
 tree_json1 <- tree_json %>%
   mutate(children = str_split(children, ", ") %>% lapply(as.numeric)) # 將children 裡的, 去掉並設為numeric 回傳list in children col 
-# lapply 會回傳list 
-# mutate(): 用mutate 可以修改 or 添加row 
+# lapply: returb a list 
+# mutate(): using mutate can either modify or add a row 
 
 tree_csv1 <- tree_csv %>%
   mutate(children = str_split(children, ", ") %>% lapply(as.numeric))
 
-# join 兩個df: tree_json1 join tree_csv1 based on id(會保留兩個df 所有id), 然後若沒匹配就輸入NA
+# join two dataframes: tree_json1 join tree_csv1 based on id (to keep all id in these two dataframes), and then enter NA if there is no match
 
 merged_df <- full_join(tree_json1, tree_csv1, by = "id", suffix = c("_tree_json1", "_tree_csv1"))
 
@@ -35,11 +37,13 @@ merged_df <- full_join(tree_json1, tree_csv1, by = "id", suffix = c("_tree_json1
 # differences bewteen parent_structure_id column and children column
 differences_basedonid <- merged_df %>%
   filter(parent_structure_id_tree_json1 != parent_structure_id_tree_csv1 |
-           !mapply(setequal, children_tree_json1, children_tree_csv1)) %>% # 在mapply 裡使用setequal 看json1 和csv1有無一樣）若一樣會是TRUE，但要找不同，所以前面加!
+           !mapply(setequal, children_tree_json1, children_tree_csv1)) %>% 
+# Using setequal in mapply to see whether json1 as same as csv1. if same, reutrn TRUE. But we are going to find different, so add ! in the begining.
   mutate(different_parent = parent_structure_id_tree_json1 != parent_structure_id_tree_csv1,
          different_children = !mapply(setequal, children_tree_json1, children_tree_csv1))
-view(differences_basedonid)
 
 # add name col in differences_basedonid df
-differences_ <- merge(differences_basedonid, df[, c("id", "name")], by = "id", all.x = TRUE) # 只取原始df 裡的id 和name. all.x 意思是第一個df 的值都會保留
+differences_json_csv <- merge(differences_basedonid, df[, c("id", "name")], by = "id", all.x = TRUE) 
+# only extract "id" and" name from df
+# all.x: meaning to keep the first value on the df
 
